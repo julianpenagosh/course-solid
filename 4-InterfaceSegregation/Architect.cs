3861395b78fb6802a555c5757868316ba86f10cd
Julian Penagos <julianpenagos37@outlook.com>
2025-04-01 18:52:26 -0500

    Add new interfaces to use interface segregation

---
+++ 4-InterfaceSegregation/Architect.cs
@@ -0,0 +1,30 @@
+namespace InterfaceSegregation
+{
+    public class Architect : IWorkTeamActivities, IDesignActivities, IDevelopActivities, ITestingActivities
+    {
+        public void Plan()
+        {
+            // Implementation for planning
+        }
+
+        public void Comunicate()
+        {
+            // Implementation for communication
+        }
+
+        public void Design()
+        {
+            // Implementation for design
+        }
+
+        public void Develop()
+        {
+            // Implementation for development
+        }
+
+        public void Test()
+        {
+            // Implementation for testing
+        }
+    }
+}
